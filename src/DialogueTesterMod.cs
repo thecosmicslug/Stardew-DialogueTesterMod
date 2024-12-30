820a7359d6113c458440f3ef21297dcb8579368c
The Cosmic Slug <thecosmicslug@gmail.com>
2024-12-30 10:24:11 +0000

    fixed for v1.6

---
+++ src/DialogueTesterMod.cs
@@ -0,0 +1,107 @@
+﻿// Stardew Dialog Testing Mod fixed for v1.6
+// Taken from https://github.com/AlanDavison/StardewValleyMods/
+
+using System;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace DialogueTester
+{
+    public class ModEntry : Mod{
+
+        private static readonly string _commandName = "dh_testdialogue";
+        private static readonly string _commandDescription = "Test some modded dialogue.";
+
+        private static readonly string _commandUsage = $"\nUsage: \t\t\t{_commandName} <Dialogue ID> <NPC name>\n" +
+        $"Advanced usage: \t{_commandName} <Full dialogue path> <NPC name> -manual\n\n" +
+        $"Regular example: \t{_commandName} summer_Wed2 Abigail\n" +
+        $"Advanced example: \t{_commandName} Characters\\Dialogue\\Abigail:summer_Wed2 Abigail -manual";
+
+        public override void Entry(IModHelper helper){
+
+            //* Add our debug command
+            helper.ConsoleCommands.Add(_commandName, $"{_commandDescription}\n\n{_commandUsage}", this.TestDialogue);
+        }
+
+        private void PrintUsage(string error){
+
+            //* Output to Debug Console
+            this.Monitor.Log(error, LogLevel.Warn);
+            this.Monitor.Log(_commandUsage, LogLevel.Warn);
+        }
+
+        private void TestDialogue(string command, string[] args){
+
+            //* We only want to do this if we're actually in-game. May not be necessary.
+            if (!Context.IsWorldReady){
+
+                this.Monitor.Log("You should only use this command once the game has loaded.", LogLevel.Info);
+                return;
+            }
+
+            string dialogueIdOrKey, npcName;
+            bool manualDialogueKey = false;
+
+            //* If we don't have the required amount of parameters, we quit.
+            // TODO: Add default NPC case if one is missing.
+            if (args.Length < 2 || args.Length > 3){
+
+                this.PrintUsage("Invalid number of arguments.");
+                return;
+            }
+
+            //* If our third argument is the -manual option, we set a bool to tell us to use the passed dialogue key verbatim.
+            if (args.Length == 3) manualDialogueKey = args[2].Equals("-manual");
+
+            //* Parameter 0 = Dialogue ID or full key
+            //* Parameter 1 = NPC name
+            //* Parameter 2 = Manual option
+            dialogueIdOrKey = args[0];
+            npcName = args[1];
+
+            var speakingNpc = Utility.fuzzyCharacterSearch(npcName);
+            string finalDialogue;
+
+            if (speakingNpc == null){
+
+                //* If the NPC doesn't exist, warn as appropriate and return.
+                this.PrintUsage("Invalid NPC name.");
+                return;
+            }
+
+            if (manualDialogueKey){
+
+                try // TODO: Make this not terrible. Works for now, though!
+                {
+                    finalDialogue = Game1.content.LoadStringReturnNullIfNotFound(dialogueIdOrKey);
+                }
+                catch (Exception)
+                {
+                    finalDialogue = null;
+                }
+
+                if (finalDialogue == null){
+                    this.PrintUsage("Invalid dialogue ID. Maybe this character doesn't have this dialogue?");
+                    return;
+                }
+            }else{
+
+                try
+                {
+                    finalDialogue = Game1.content.LoadStringReturnNullIfNotFound($"Characters\\Dialogue\\{npcName}:{dialogueIdOrKey}");
+                }
+                catch (Exception)
+                {
+                    finalDialogue = null;
+                }
+
+                if (finalDialogue == null){
+                    this.PrintUsage("Invalid dialogue ID. Maybe this character doesn't have this dialogue?");
+                    return;
+                }
+            }
+
+            Game1.DrawDialogue(new Dialogue(speakingNpc, null, finalDialogue));
+        }
+    }
+}
