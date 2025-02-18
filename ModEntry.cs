731098615018c8a1ee90ab4a1d24bd88cc49fc75
The Cosmic Slug <thecosmicslug@gmail.com>
2025-02-18 17:55:45 +0000

    add translation support

--- ModEntry.cs
+++ ModEntry.cs
@@ -13,21 +13,6 @@ namespace DialogueTester
 {
     public class ModEntry : Mod {
 
-        //* Strings
-        //TODO: Add translation support.
-        //use like = i18n.GetTranslation("Config.CarolineSprite");
-        private static readonly string _commandName = "dialogue_tester";
-        private static readonly string _commandHelp = "dialogue_tester_help";
-        private static readonly string _commandDescription = "Test some dialogue while modding, show which translation key is used ingame.";
-        private static readonly string _InvalidDialogue = "Invalid dialogue ID. Maybe this character doesn't have this dialogue?";
-        private static readonly string _InvalidArguments = "Invalid number of arguments.";
-        private static readonly string _InvalidNPC = "Invalid NPC name.";
-
-        private static readonly string _commandUsage = $"\nUsage: \t\t\t{_commandName} <Dialogue ID> <NPC name>\n" +
-        $"Advanced usage: \t{_commandName} <Full dialogue path> <NPC name> -manual\n\n" +
-        $"Regular example: \t{_commandName} summer_Wed2 Abigail\n" +
-        $"Advanced example: \t{_commandName} Characters\\Dialogue\\Abigail:summer_Wed2 Abigail -manual\n";
-
         //* Logging
         private static IMonitor monitor;
         private static bool bIgnore = false;
@@ -41,21 +26,21 @@ namespace DialogueTester
             try{
                 //* Load configuration
                 this.config = this.Helper.ReadConfig<ModConfig>();
-                Monitor.Log("Configuration loaded.", LogLevel.Info);
+                i18n.gethelpers(this.Helper.Translation, this.config);
+                Monitor.Log(i18n.GetTranslation("DialogueTester_LoadedConfig"), LogLevel.Info);
             }
             catch{
                 //* Create configuration
-                Monitor.Log("No config found, using defaults.", LogLevel.Info);
+                i18n.gethelpers(this.Helper.Translation, this.config);
+                Monitor.Log(i18n.GetTranslation("DialogueTester_NoConfig"), LogLevel.Info);
                 this.config = new ModConfig();
             }
-            
-            //* Setup Logging, Translation Support.
-            monitor = Monitor;
-            i18n.gethelpers(this.Helper.Translation, this.config);
 
-            //* Add our debug command
-            helper.ConsoleCommands.Add(_commandName, $"{_commandDescription}\n\n{_commandUsage}", this.TestDialogue);
-            helper.ConsoleCommands.Add(_commandHelp, "Show Help", this.PrintUsage);
+            monitor = Monitor;
+
+            //* Add our debug commands
+            helper.ConsoleCommands.Add(i18n.GetTranslation("DialogueTester_commandName"), i18n.GetTranslation("DialogueTester_commandDescription") + i18n.GetTranslation("DialogueTester_commandUsage", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}) + i18n.GetTranslation("DialogueTester_commandUsageAdv", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}), this.TestDialogue);
+            helper.ConsoleCommands.Add(i18n.GetTranslation("DialogueTester_commandHelp"), i18n.GetTranslation("DialogueTester_commandDescription"), this.PrintUsage);
 
             //* Setup Harmony
             if (config.EnableHarmony){
@@ -64,7 +49,6 @@ namespace DialogueTester
 
             //* Setup Event Hooks
             helper.Events.GameLoop.GameLaunched += onLaunched;
-            Monitor.Log("Setup is complete.", LogLevel.Info);
         }
         
         //* Setup for GenericModConfigMenu support.
@@ -77,13 +61,13 @@ namespace DialogueTester
                 api.Register(this.ModManifest, () => this.config = new ModConfig(), () => Helper.WriteConfig(this.config));
                     
                 //* Our Options
-                api.AddSectionTitle(this.ModManifest,() => "Harmony Settings", () => "DialogueTester can display the keys used when NPCs talk in-game if we use a Harmony postfix." );
-                api.AddBoolOption(this.ModManifest,() => this.config.EnableHarmony, (bool val) => this.config.EnableHarmony = val, ()  => "Enable Harmony support.", () => "Will log dialogue keys to the console when used.","EnableHarmony");
-                api.AddBoolOption(this.ModManifest,() => this.config.IgnoreSelf, (bool val) => this.config.IgnoreSelf = val, ()  => "Ignore Dialogue Requested.", () => "Don't bother logging dialogue we have asked for.","IgnoreSelf");
+                api.AddSectionTitle(this.ModManifest,() => i18n.GetTranslation("Config_HarmonySettingTitle"), () => i18n.GetTranslation("Config_HarmonyDescription"));
+                api.AddBoolOption(this.ModManifest,() => this.config.EnableHarmony, (bool val) => this.config.EnableHarmony = val, ()  => i18n.GetTranslation("Config_EnableHarmony"), () => i18n.GetTranslation("Config_HarmonyDescription"),"EnableHarmony");
+                api.AddBoolOption(this.ModManifest,() => this.config.IgnoreSelf, (bool val) => this.config.IgnoreSelf = val, ()  => i18n.GetTranslation("Config_IgnoreSelfSetting"), () => i18n.GetTranslation("Config_IgnoreSelfDescription"));
 
                 //* Detect changes mid-game.
                 api.OnFieldChanged(this.ModManifest,onFieldChanged);
-                monitor.Log("GenericModConfigMenu setup complete.", LogLevel.Info);
+                monitor.Log(i18n.GetTranslation("DialogueTester_GMCMSetup"), LogLevel.Info);
             }
 
         }
@@ -114,11 +98,10 @@ namespace DialogueTester
             }
         }
 
-        //* Enable Harmony Patching
         private void PatchHarmony(){
             
             //* Setup Harmony
-            monitor.Log("Setting up harmony postfix...", LogLevel.Info);
+            monitor.Log(i18n.GetTranslation("DialogueTester_SetupHarmony"), LogLevel.Info);
             Harmony harmony = new Harmony(ModManifest.UniqueID);
 
             //* Patch DialogBox to find the translation keys used.
@@ -129,10 +112,10 @@ namespace DialogueTester
 
         }
 
-        //* Remove Harmony Patching
         private void DisableHarmony(){
 
-            monitor.Log("removing harmony postfix...", LogLevel.Info);
+            //* Remove Harmony Patching
+            monitor.Log(i18n.GetTranslation("DialogueTester_ShutdownHarmony"), LogLevel.Info);
             Harmony harmony = new Harmony(ModManifest.UniqueID);
             harmony.UnpatchAll(harmony.Id);
         }
@@ -146,26 +129,27 @@ namespace DialogueTester
                     bIgnore = false;
                     return;
                 }
-
                 //* Log NPC
                 monitor.Log($"NPC: {dialogue.speaker.Name}", LogLevel.Warn);
                 //* There isn't always a key.
                 if (dialogue.TranslationKey is not null){
                     monitor.Log($"Key: {dialogue.TranslationKey}", LogLevel.Warn);
                 }
-                //* Log dialogue.
-                //TODO: Loop all dialogues?
-                monitor.Log($"Dialogue: {dialogue.dialogues[dialogue.currentDialogueIndex].Text}", LogLevel.Warn);
+                //* Log dialogue(s).
+                for (int i = 0; i < dialogue.dialogues.Count; i++){
+                    monitor.Log($"Dialogue[{i}]: {dialogue.dialogues[i].Text}", LogLevel.Warn);
+                }
+                
             }
             //* ERROR!
             catch (Exception e) {
-                monitor.Log($"Failed in {nameof(Dialogue_Postfix)}:\n{e}", LogLevel.Error);
+                monitor.Log($"{i18n.GetTranslation("DialogueTester_PostfixError")} {nameof(Dialogue_Postfix)}:\n{e}", LogLevel.Error);
             }
         }
 
         private void PrintUsage(string command, string[] args) {
             //* Output to Debug Console
-            monitor.Log(_commandUsage, LogLevel.Info);
+            monitor.Log(i18n.GetTranslation("DialogueTester_commandUsage", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}) + i18n.GetTranslation("DialogueTester_commandUsageAdv", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}) + i18n.GetTranslation("DialogueTester_commandExample", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}) + i18n.GetTranslation("DialogueTester_commandExampleAdv", new {_commandName = i18n.GetTranslation("DialogueTester_commandName")}), LogLevel.Info);
         }
 
         private void TestDialogue(string command, string[] args) {
@@ -180,7 +164,7 @@ namespace DialogueTester
 
             //* If we don't have the required amount of parameters, we quit.
             if (args.Length < 2 || args.Length > 3){
-                monitor.Log(_InvalidArguments, LogLevel.Error);
+                monitor.Log(i18n.GetTranslation("DialogueTester_InvalidArguments"), LogLevel.Error);
                 return;
             }
 
@@ -198,7 +182,7 @@ namespace DialogueTester
 
             if (speakingNpc == null){
                 //* If the NPC doesn't exist, warn as appropriate and return.
-                monitor.Log(_InvalidNPC, LogLevel.Error);
+                monitor.Log(i18n.GetTranslation("DialogueTester_InvalidNPC"), LogLevel.Error);
                 return;
             }
 
@@ -214,7 +198,7 @@ namespace DialogueTester
                 }
                 if (finalDialogue == null){
                     //* FAIL NOT FOUND
-                    monitor.Log(_InvalidDialogue, LogLevel.Error);
+                    monitor.Log(i18n.GetTranslation("DialogueTester_InvalidDialogue"), LogLevel.Error);
                     return;
                 }
             }else{
@@ -229,7 +213,7 @@ namespace DialogueTester
                 }
                 if (finalDialogue == null){
                     //* FAIL NOT FOUND
-                    monitor.Log(_InvalidDialogue, LogLevel.Error);
+                    monitor.Log(i18n.GetTranslation("DialogueTester_InvalidDialogue"), LogLevel.Error);
                     return;
                 }
             }
